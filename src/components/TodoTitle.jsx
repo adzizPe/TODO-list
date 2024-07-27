148d3957fce174912e3b5c06616bd83afaa56b4a
adzizPe <adzanaziz2@gmail.com>
2024-07-27 19:09:14 +0700

    commit update

--- src/components/TodoTitle.jsx
+++ src/components/TodoTitle.jsx
@@ -1,10 +1,12 @@
-
 import React from "react";
 
 const TodoTitle = () => {
   return (
-    <div className="todo-title">{<h1>Apa rencana untuk hari ini?</h1>}
-    
+    <div className="todo-title">
+      <video autoPlay loop style={{ position: "absolute", width: "100%", left: 0, top: 0, zIndex: -1 }}>
+        <source src="/src/assets/img/mycat-brenxdan.mp4" type="video/mp4" />
+      </video>
+      <h1>Apa rencana untuk hari ini?</h1>
     </div>
   );
 };
